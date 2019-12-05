7b555fd63af1d4d3932d3164107c8d9f73545e1d
Caioww <sousinhacaio@gmail.com>
2019-12-05 00:54:40 -0300

    finalizando projeto

---
+++ imc_flutter/lib/main.dart
@@ -0,0 +1,12 @@
+import 'dart:io' show Platform;
+import 'package:flutter/material.dart';
+import 'package:imc_flutter/ui/android/material-app.dart';
+import 'package:imc_flutter/ui/ios/cupertino-app.dart';
+
+
+void main() => 
+    Platform.isAndroid ? runApp(MyMaterialApp()) : runApp(MyCupertinoApp());
+
+
+
+
